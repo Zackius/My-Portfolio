e844709f009aa399d094ec2eff76997d57e34794
Zacharia <ndunguzachary24@gmail.com>
2022-11-09 00:29:04 +0300

    Work link added

--- src/components/Work.js
+++ src/components/Work.js
@@ -61,7 +61,7 @@ const Work = () => {
             Recipe Application
               </span>
               <div className="pt-8  text-center">
-                <a href="/">
+                <a href="https://tastebite-recipe-app.vercel.app/">
                   <button className="text-center rounded-lg  px-4 py-3  m-2  bg-white text-gray-600 font-bold text-lg">
                     Demo
                   </button>
